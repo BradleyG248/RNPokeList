3d4190342e36e7818c9289ba55ee3ed7c8bd1dda
BradleyG248 <calvinandhobbs28@gmail.com>
2020-10-02 13:39:45 -0600

    Capitalized, API requests working

--- Components/Home.js
+++ Components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import axios from 'axios';
 
-export default class Home extends Component {
+export default class Home extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -15,21 +15,23 @@ export default class Home extends Component {
   }
   getPokemon = async () => {
     let res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=10');
-    this.setState({ pokemon: res.data });
+    this.setState({ pokemon: res.data.results });
     this.renderPokemon();
   }
   renderPokemon = () => {
     let newComponents = [];
-    this.pokemon.forEach(pokemon => {
-      console.log(pokemon.name);
-      newComponents.push(<View style={styles.pokemonCard}>{pokemon.name}</View>)
+    this.state.pokemon.forEach(pokemon => {
+      pokemon.name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+      newComponents.push(<View style={styles.pokemonCard}><Text>{pokemon.name}</Text></View>)
     })
+    this.setState({ PokeComponents: newComponents })
   }
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
+        {this.state.PokeComponents}
       </View>
     );
   }
